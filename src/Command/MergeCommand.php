0cd7424c68b3c38cbb94a5aaeafbd2e8672546c7
Martin Auswöger <martin@auswoeger.com>
2018-08-06 09:14:29 +0200

    Use the Contao bundles

--- src/Command/MergeCommand.php
+++ src/Command/MergeCommand.php
@@ -29,11 +29,25 @@ class MergeCommand extends Command
     {
         $merger = new Merger(
             \dirname(__DIR__, 2).'/test-repos/mono/.git',
+            /*
             [
                 'faq-bundle' => \dirname(__DIR__, 2).'/test-repos/faq-bundle/.git',
                 'listing-bundle' => \dirname(__DIR__, 2).'/test-repos/listing-bundle/.git',
             ],
+            */
             [
+                'calendar-bundle' => 'git@github.com:contao/calendar-bundle.git',
+                'comments-bundle' => 'git@github.com:contao/comments-bundle.git',
+                'core-bundle' => 'git@github.com:contao/core-bundle.git',
+                'faq-bundle' => 'git@github.com:contao/faq-bundle.git',
+                'installation-bundle' => 'git@github.com:contao/installation-bundle.git',
+                'listing-bundle' => 'git@github.com:contao/listing-bundle.git',
+                'manager-bundle' => 'git@github.com:contao/manager-bundle.git',
+                'news-bundle' => 'git@github.com:contao/news-bundle.git',
+                'newsletter-bundle' => 'git@github.com:contao/newsletter-bundle.git',
+            ],
+            [
+                /*
                 // Listing
                 'dea5b1bf4913dfc67961b2352936fcf8abbf88fb',
                 'e4fe9c298575eefdef2b4a4db00618293be1186c',
@@ -45,14 +59,8 @@ class MergeCommand extends Command
                 '72417c996b78f139d35488a2589e91ce049a868e',
                 '332b5d7e98c406814a7b734c766c29bd5d4f4fba',
                 'ce9f2b63c397f82079faa2c473fc923555ad0f90',
+                */
             ],
-            /*
-            [
-                'sub-a' => 'git@github.com:ausi/slug-generator.git',
-                'sub-b' => 'git@github.com:ausi/slug-generator.git',
-            ],
-            [],
-            */
             \dirname(__DIR__, 2).'/merge-cache',
             $output
         );
