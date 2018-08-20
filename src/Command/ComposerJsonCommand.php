fe804427359106a0f200c32544796f14679100ee
Martin Auswöger <martin@auswoeger.com>
2018-08-20 19:14:09 +0200

    Remove unnecessary fallback array

--- src/Command/ComposerJsonCommand.php
+++ src/Command/ComposerJsonCommand.php
@@ -189,7 +189,7 @@ class ComposerJsonCommand extends Command
 
         $rootJson['conflict'] = $this->combineDependecies(array_map(function($json) {
             return $json['conflict'] ?? [];
-        }, $jsons), array_keys($rootJson['replace']) ?? []);
+        }, $jsons), array_keys($rootJson['replace']));
 
         $rootJson['bin'] = $this->combineBins(array_map(function($json) {
             return $json['bin'] ?? [];
