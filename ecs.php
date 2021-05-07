1bbe4db88bb47887cc07a42bff9f1d63462dba06
Martin Auswöger <martin@auswoeger.com>
2021-05-07 18:18:19 +0200

    Coding standard

---
+++ ecs.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__.'/vendor/contao/easy-coding-standard/config/set/contao.php');
+
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SKIP, [
+        MethodChainingIndentationFixer::class => [
+            '*/Config/MonorepoConfiguration.php',
+        ],
+    ]);
+};
